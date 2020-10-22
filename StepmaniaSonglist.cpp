1f862d421cab25efc0b32e08635d9be88660677c
StarlightLumi <todd.oakes@gmail.com>
2020-10-22 01:06:48 -0500

    v1.0 tidy-up
    oops left in the default visual studio blurb hehe

--- StepmaniaSonglist.cpp
+++ StepmaniaSonglist.cpp
@@ -1,9 +1,11 @@
-// StepmaniaSonglist.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-// TO GET THE SONGLIST THIS FILE USES, OPEN UP YOUR \Stepmania 5\Songs\ folder in the command prompt and run:
-// dir /s /b *.sm > songlist.txt
-// this program assumes you've already got a songlist.txt ready to go.
-// lets get it!
+// Code by Todd Oakes, 10/22/2020
+// Free to use and improve, so long as Todd Oakes is credited as well.
+
+// It's essentially a fancy file reader.
+// Since .sm files use # tags, finding the info we need is easy.
+// However, the "Locale" doesnt work.
+// Sometimes UTF characters get dropped.
+//  but not always?  And usually only from titles?
 
 #include <iostream>
 #include <fstream>
@@ -92,25 +94,6 @@ int main()
         chartToRead.close();
     }
     csvoutput.close();
-    //cin.getline(path, MAXTOREAD);
-  
-    //  ask user for a filename (testing purposes)
-    //cin.getline(filename, MAXTOREAD);
-
-
-
-    
-
+ 
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
